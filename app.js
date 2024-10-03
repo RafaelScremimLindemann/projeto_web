99d2e2b7ffcb9f7b8cafe624eccdfac374728409
Rafael Scremim <rafaelscremim1997@gmail.com>
2024-10-02 21:22:31 -0300

    primeiro envio

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+// Servir arquivos estáticos da pasta "public"
+app.use(express.static('public'));
+
+// Middleware para interpretar JSON no corpo das requisições
+app.use(express.json());
+
+// Rotas (adicione suas rotas aqui)
+app.use('/users', require('./routes/users'));
+
+// Iniciar o servidor
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
+
+//sempre acessar a pasta (cd) onde está o projeto para executá-lo
+//ou botao direito no projeto open in integrated terminal
---
+++ controllers/userController.js
@@ -0,0 +1,25 @@
+const db = require('../models/db');
+
+// Buscar todos os usuários
+exports.getAllUsers = async (req, res) => {
+    try {
+        const [users] = await db.query('SELECT * FROM users');
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+
+const bcrypt = require('bcrypt');
+// Criar um novo usuário
+exports.createUser = async (req, res) => {
+    const { nome, email, senha } = req.body;
+    try {
+        const hashedPassword = await bcrypt.hash(senha,10);
+        const result = await db.query('INSERT INTO users (nome, email, senha) VALUES (?, ?, ?)', [nome, email, hashedPassword]);
+        res.json({ message: 'Usuário criado com sucesso!', userId: result[0].insertId });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
